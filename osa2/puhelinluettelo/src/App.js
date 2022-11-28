88bae1000819bb693af5d88a31c6985129f12a5d
arttumikkola <arttu.mikkola@windowslive.com>
2022-11-28 14:38:15 +0200

    2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,94 +1,35 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import nameService from "./services/names";
+import "./App.css";
 
-const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [newFilter, setNewFilter] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/db").then((response) => {
-      setPersons(response.data.persons);
-    });
-  }, []);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    };
-
-    if (
-      !persons.some(
-        (person) => person.name.toLowerCase() === newName.toLowerCase()
-      )
-    ) {
-      setPersons(persons.concat(nameObject));
-      setNewName("");
-      setNewNumber("");
-    } else {
-      window.alert(`${newName} is already added to the phonebook`);
-    }
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value);
-  };
-
-  const personsToShow = persons.filter((person) =>
-    person.name.toLowerCase().includes(newFilter.toLowerCase())
-  );
-
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <FilterForm value={newFilter} onChange={handleFilterChange} />
-      <h2>add a new</h2>
-      <PersonForm
-        onSubmit={addNote}
-        nameInputValue={newName}
-        onNameChange={handleNameChange}
-        numberInputValue={newNumber}
-        onNumberChange={handleNumberChange}
-      />
-      <h2>Numbers</h2>
-      {personsToShow.map((person) => (
-        <Person person={person} key={person.id} />
-      ))}
-    </div>
-  );
-};
-
-const FilterForm = (props) => {
+const FilterForm = ({ newFilter, setNewFilter }) => {
   return (
     <div>
       filter shown with
-      <input value={props.value} onChange={props.onChange} />
+      <input value={newFilter} onChange={(e) => setNewFilter(e.target.value)} />
     </div>
   );
 };
 
-const PersonForm = (props) => {
+const PersonForm = ({
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+  addPerson,
+}) => {
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={(e) => addPerson(e)}>
       <div>
         name:{" "}
-        <input value={props.nameInputValue} onChange={props.onNameChange} />
+        <input value={newName} onChange={(e) => setNewName(e.target.value)} />
       </div>
       <div>
         number:{" "}
-        <input value={props.numberInputValue} onChange={props.onNumberChange} />
+        <input
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
+        />
       </div>
       <div>
         <button type="submit">add</button>
@@ -97,11 +38,191 @@ const PersonForm = (props) => {
   );
 };
 
-const Person = (props) => {
+const Person = ({ person }) => {
   return (
     <p>
-      {props.person.name} {props.person.number}
+      {person.name} {person.number}
     </p>
+  );
+};
+
+const Persons = ({ deleteName, persons, newFilter }) => {
+  return (
+    <div>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(newFilter.toLowerCase())
+        )
+        .map((person) => (
+          <div key={person.id}>
+            <Person person={person} />
+            <button onClick={() => deleteName(person.id, person.name)}>
+              delete
+            </button>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const Notification = ({ message, messageType }) => {
+  if (message === null) {
+    return null;
+  }
+  if (messageType === "success") {
+    return <div className="success">{message}</div>;
+  } else if (messageType === "error") {
+    return <div className="error">{message}</div>;
+  }
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+  const [message, setMessage] = useState(null);
+  const [messageType, setMessageType] = useState(null);
+
+  useEffect(() => {
+    const getPersons = async () => {
+      await nameService.getAll().then((initialPersons) => {
+        setPersons(initialPersons.db);
+      });
+    };
+    getPersons();
+  }, []);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: getTitleCase(newName),
+      number: newNumber,
+    };
+
+    const found = persons.find(
+      (person) => person.name === getTitleCase(newName)
+    );
+
+    if (!found && (newNumber != null || newNumber != undefined)) {
+      nameService.create(nameObject).then((returnedName) => {
+        setPersons(persons.concat(returnedName));
+        setNewName("");
+        setNewNumber("");
+        setMessageType("success");
+        setMessage(`Added ${getTitleCase(newName)}`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 2000);
+      });
+    } else {
+      try {
+        if (
+          window.confirm(
+            `${newName} is already in your phonebook, do you want to replace the old number?`
+          )
+        ) {
+          setPersons(updatePersons(nameObject));
+          setNewName("");
+          setNewNumber("");
+          setMessageType("success");
+          setMessage(`Updated ${getTitleCase(newName)}'s number`);
+          setTimeout(() => {
+            setMessage(null);
+          }, 2000);
+        }
+      } catch (err) {
+        setMessageType("error");
+        setMessage(err.response.data.error);
+        setTimeout(() => {
+          setMessage(null);
+        }, 2000);
+      }
+    }
+  };
+
+  const updatePersons = (obj, found) => {
+    try {
+      nameService.update(found.id, obj);
+      const index = persons.findIndex((person) => {
+        return person.id === found.id;
+      });
+      const persons2 = [...persons];
+      persons2[index] = {
+        id: found.id,
+        name: obj.name,
+        number: obj.number,
+      };
+      return persons2;
+    } catch (err) {
+      setMessageType("error");
+      setMessage(
+        `${getTitleCase(obj.name)} has already been deleted from the server.`
+      );
+      setTimeout(() => {
+        setMessage(null);
+      }, 2000);
+      setPersons(persons.filter((person) => person.id != found.id));
+    }
+  };
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${getTitleCase(name)} ?`)) {
+      try {
+        nameService.remove(id);
+        setPersons(
+          persons.filter((person) => {
+            return person.id !== id;
+          })
+        );
+        setMessageType("success");
+        setMessage(`Deleted ${name}`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 2000);
+      } catch (err) {
+        setMessageType("error");
+        setMessage(err.response.data.error);
+        setTimeout(() => {
+          setMessage(null);
+        }, 2000);
+      }
+    }
+  };
+
+  function getTitleCase(str) {
+    const titleCase = str
+      .toLowerCase()
+      .split(" ")
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+
+    return titleCase;
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Notification message={message} messageType={messageType} />
+      <FilterForm filter={newFilter} setNewFilter={setNewFilter} />
+      <h2>add a new name</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        addPerson={addPerson}
+      />
+      <h2>Numbers</h2>
+      <Persons
+        newFilter={newFilter}
+        persons={persons}
+        deleteName={deletePerson}
+      />
+    </div>
   );
 };
 
