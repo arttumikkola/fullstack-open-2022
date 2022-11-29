1256e711411ade3eaf7f236e7a6f581e791b22bc
arttumikkola <arttu.mikkola@windowslive.com>
2022-11-29 15:18:32 +0200

    .

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,29 +1,66 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const Country = ({ filteredCountries }) => {
+  if (filteredCountries.length === 1) {
+    return (
+      <div>
+        <h1> {filteredCountries[0].name.common} </h1>
+        <p>capital: {filteredCountries[0].capital} </p>
+        <p>area: {filteredCountries[0].area} </p>
+        <b>languages:</b>
+        <ul>
+          {Object.values(filteredCountries[0].languages).map((value, index) => {
+            return <li key={index}>{value}</li>;
+          })}
+        </ul>
+        <img src={filteredCountries[0].flags.svg} alt="flag" width="200"></img>
+        <p>temperature: {/* weather.main.temp */} </p>
+        <h1>Weather in {filteredCountries[0].capital}</h1>
+        <p>wind: {/* {weather.main.wind} */} </p>
+      </div>
+    );
+  }
+};
+
+const CountriesToShow = ({ input, filteredCountries, setInput, countries }) => {
+  if (filteredCountries.length > 11) {
+    return <div></div>;
+  } else if (filteredCountries.length > 10 && input.length > 0) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (1 < filteredCountries.length && filteredCountries.length < 10) {
+    return filteredCountries.map((country) => (
+      <div key={country.name.common}>
+        {country.name.common}
+        <button onClick={() => setInput(country.name.common)}>show</button>
+      </div>
+    ));
+  } else if (filteredCountries.length === 1) {
+    return <Country countries={countries} />;
+  }
+};
+
 const App = () => {
-  const [countryInput, setCountryInput] = useState("");
+  const [input, setInput] = useState("");
   const [countries, setCountries] = useState([]);
-  const [show, setShow] = useState("");
-  const [weather, setWeather] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  /* const [weather, setWeather] = useState([]); */
 
   const apiKey = "289126ef1b80a9dd3fd33c17f6d4035a";
   //https://api.openweathermap.org/data/2.5/weather?q={apikey}&appid={apikey}
 
   useEffect(() => {
-    if (countryInput.length > 0) {
-      axios
-        .get(`https://restcountries.com/v3.1/name/${countryInput}`)
-        .then((response) => {
-          setCountries(response.data);
-          if (1 < countries.length < 10) {
-            setCountries(response.data);
-          } else if (countries.length === 1) {
-            setCountries(response.data);
-          }
-        });
-    }
-  });
+    const fetch = async () => {
+      const { data } = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(data);
+    };
+    fetch();
+  }, []);
+
+  useEffect(() => {
+    const filter = countries.filter((country) => country.includes(input));
+    setFilteredCountries(filter);
+  }, [countries, input]);
 
   /*  useEffect(() => {
     if (countries.length === 1) {
@@ -37,56 +74,15 @@ const App = () => {
     }
   }); */
 
-  const handleInputChange = (event) => {
-    setCountryInput(event.target.value);
-  };
-
-  const Country = () => {
-    return (
-      <div>
-        <h1> {countries[0].name.common} </h1>
-        <p>capital: {countries[0].capital} </p>
-        <p>area: {countries[0].area} </p>
-        <b>languages:</b>
-        <ul>
-          {Object.values(countries[0].languages).map((value, index) => {
-            return <li key={index}>{value}</li>;
-          })}
-        </ul>
-        <img src={countries[0].flags.svg} alt="flag" width="200"></img>
-        <h1>Weather in {countries[0].capital}</h1>
-        <p>temperature: {/* weather.main.temp */} </p>
-        <p>wind: {/* {weather.main.wind} */} </p>
-      </div>
-    );
-  };
-
-  const CountriesToShow = () => {
-    if (countryInput.length === 0) {
-      return <div></div>;
-    } else if (countries.length > 10 && countryInput.length > 0) {
-      return <p>Too many matches, specify another filter</p>;
-    } else if (1 < countries.length && countries.length < 10) {
-      return countries.map((country, i) => (
-        <div key={i}>
-          {country.name.common}
-          <button onClick={showCountry(i)}>show</button>
-        </div>
-      ));
-    } else if (countries.length === 1) {
-      return <Country />;
-    }
-  };
-
-  const showCountry = (i) => {
-    setCountries(countries[i - 1].name.common);
-  };
-
   return (
     <div>
       <p>find countries</p>
-      <input value={countryInput} onChange={handleInputChange} />
-      <CountriesToShow />
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <CountriesToShow
+        input={input}
+        countries={countries}
+        filteredCountries={filteredCountries}
+      />
     </div>
   );
 };
