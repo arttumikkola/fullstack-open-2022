d95dab6e4816e972539c456a7f4635d0dc61988e
arttumikkola <arttu.mikkola@windowslive.com>
2022-12-01 13:45:49 +0200

    2.14

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,8 +1,59 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const App = () => {
+  const [input, setInput] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(data);
+    };
+    fetch();
+  }, []);
+
+  useEffect(() => {
+    const filter = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(input.toLowerCase())
+    );
+    setFilteredCountries(filter);
+  }, [countries, input]);
+
+  return (
+    <div>
+      <p>find countries</p>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <CountriesToShow
+        input={input}
+        countries={countries}
+        filteredCountries={filteredCountries}
+        setInput={setInput}
+      />
+    </div>
+  );
+};
+
+export default App;
+
 const Country = ({ filteredCountries }) => {
-  if (filteredCountries.length === 1) {
+  const [weather, setWeather] = useState();
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (filteredCountries.length == 1) {
+        const { data } = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${filteredCountries[0].capital}&appid=${apiKey}`
+        );
+        setWeather(data);
+      }
+    };
+    fetch();
+  }, [filteredCountries.length]);
+
+  if (weather != undefined) {
     return (
       <div>
         <h1> {filteredCountries[0].name.common} </h1>
@@ -15,15 +66,20 @@ const Country = ({ filteredCountries }) => {
           })}
         </ul>
         <img src={filteredCountries[0].flags.svg} alt="flag" width="200"></img>
-        <p>temperature: {/* weather.main.temp */} </p>
         <h1>Weather in {filteredCountries[0].capital}</h1>
-        <p>wind: {/* {weather.main.wind} */} </p>
+        <p>temperature: {(weather.main.temp - 273.15).toFixed(2)} °C </p>
+        <img
+          src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+        />
+        <p>wind: {weather.wind.speed.toFixed(2)} m/s </p>
       </div>
     );
+  } else {
+    return <div></div>;
   }
 };
 
-const CountriesToShow = ({ input, filteredCountries, setInput, countries }) => {
+const CountriesToShow = ({ input, filteredCountries, setInput, weather }) => {
   if (filteredCountries.length > 11) {
     return <div></div>;
   } else if (filteredCountries.length > 10 && input.length > 0) {
@@ -36,55 +92,6 @@ const CountriesToShow = ({ input, filteredCountries, setInput, countries }) => {
       </div>
     ));
   } else if (filteredCountries.length === 1) {
-    return <Country countries={countries} />;
+    return <Country filteredCountries={filteredCountries} weather={weather} />;
   }
 };
-
-const App = () => {
-  const [input, setInput] = useState("");
-  const [countries, setCountries] = useState([]);
-  const [filteredCountries, setFilteredCountries] = useState([]);
-  /* const [weather, setWeather] = useState([]); */
-
-  const apiKey = "289126ef1b80a9dd3fd33c17f6d4035a";
-  //https://api.openweathermap.org/data/2.5/weather?q={apikey}&appid={apikey}
-
-  useEffect(() => {
-    const fetch = async () => {
-      const { data } = await axios.get("https://restcountries.com/v3.1/all");
-      setCountries(data);
-    };
-    fetch();
-  }, []);
-
-  useEffect(() => {
-    const filter = countries.filter((country) => country.includes(input));
-    setFilteredCountries(filter);
-  }, [countries, input]);
-
-  /*  useEffect(() => {
-    if (countries.length === 1) {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${countries[0].capital}&appid=${apiKey}`
-        )
-        .then((response) => {
-          setWeather(response.data);
-        });
-    }
-  }); */
-
-  return (
-    <div>
-      <p>find countries</p>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <CountriesToShow
-        input={input}
-        countries={countries}
-        filteredCountries={filteredCountries}
-      />
-    </div>
-  );
-};
-
-export default App;
